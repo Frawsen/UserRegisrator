37cce19bf71734a6d4c326502b038dd3da191198
Frawsen <143500474+Frawsen@users.noreply.github.com>
2024-03-30 17:10:03 +0100

    test

--- UserRegisrator/UnitTest1.cs
+++ UserRegisrator/UnitTest1.cs
@@ -1,11 +1,48 @@
+using UserRegisClass;
+
 namespace UserRegisrator
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestIfUserNameistaken()
         {
+            //arrange 
+
+            var user1 = new User("Fredrik", "fredrik@hotmail.com", "Fredrik123@");
+            var user2 = new User("Fredrik", "fredrik@hotmail.com", "Fredrik123@");
+            var UserCollections = new UserCollections();
+
+
+            //act 
+
+            UserCollections.UserAdding(user1);
+
+
+            // assert
+
+            Assert.ThrowsException<ArgumentException>(() => UserCollections.UserAdding(user2));
+
+
+        }
+
+
+        [TestMethod]
+        public void testifNameisunder5andover20Character()
+        {
+            //arrange 
+
+       
+
+
+
+
+            //act  // assert  
+
+            Assert.ThrowsException<ArgumentException>(() => new User("Fre", "fredrik@hotmail.com", "Fredrik123@"));
+            Assert.ThrowsException<ArgumentException>(() => new User("Fredrikkkfkfkfkfkfkfkfkfkfkfkfkfkfkfkfkkfkffkfkfkfk", "fredrik@hotmail.com", "Fredrik123@"));
+
         }
     }
 }
